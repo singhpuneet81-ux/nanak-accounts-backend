dac0372cb9df3ecee83faf9e26cfe1d359ed7fff
shiva-Travelodeal <shivanshunigam01@gmail.com>
2026-03-20 17:41:54 +0530

    test

--- src/routes/bookkeepingPricingRoutes.js
+++ src/routes/bookkeepingPricingRoutes.js
@@ -1,10 +1,9 @@
-
 const router = require("express").Router();
 const { protect } = require("../middleware/auth");
 const bookkeepingController = require("../controllers/bookkeepingPricing.controller");
 const { seed: bookkeepingSeed } = require("../seeds/bookkeepingPricing.seed");
 
-router.get("/", protect, bookkeepingController.getAll);
+router.get("/", bookkeepingController.getAll);
 router.put("/", protect, bookkeepingController.upsert);
 router.delete("/", protect, bookkeepingController.remove);
 router.post("/seed", protect, bookkeepingSeed);
--- src/routes/payrollPricingRoutes.js
+++ src/routes/payrollPricingRoutes.js
@@ -1,13 +1,11 @@
-
 const router = require("express").Router();
 const { protect } = require("../middleware/auth");
 const payrollController = require("../controllers/payrollPricing.controller");
 const { seed: payrollSeed } = require("../seeds/payrollPricing.seed");
 
-router.get("/", protect, payrollController.getAll);
+router.get("/", payrollController.getAll);
 router.put("/", protect, payrollController.upsert);
 router.delete("/", protect, payrollController.remove);
 router.post("/seed", protect, payrollSeed);
 
 module.exports = router;
-
