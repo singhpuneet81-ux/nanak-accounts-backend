60b5d49fa05c316e8f6e21273c3d2c4c46511f55
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2026-03-20 16:59:17 +0530

    test

--- src/controllers/payrollPricing.controller.js
+++ src/controllers/payrollPricing.controller.js
@@ -1,40 +1,35 @@
+const PayrollPricing = require("../models/PayrollPricing.model");
 
-
-// const PayrollPricing = require("../models/PayrollPricing.model");
-
-const payrollController = {
-  getAll: async (req, res) => {
-    try {
-      const docs = await PayrollPricing.find().lean();
-      res.json({ success: true, data: docs });
-    } catch (err) {
-      res.status(500).json({ success: false, message: err.message });
-    }
-  },
-
-  upsert: async (req, res) => {
-    try {
-      const data = req.body;
-      if (!data.serviceKey)
-        return res.status(400).json({ success: false, message: "serviceKey required" });
-      const doc = await PayrollPricing.findOneAndUpdate(
-        { serviceKey: data.serviceKey },
-        { $set: data },
-        { upsert: true, new: true, runValidators: true }
-      );
-      res.json({ success: true, data: doc });
-    } catch (err) {
-      res.status(500).json({ success: false, message: err.message });
-    }
-  },
-
-  remove: async (req, res) => {
-    try {
-      await PayrollPricing.deleteMany({});
-      res.json({ success: true, message: "All payroll pricing deleted" });
-    } catch (err) {
-      res.status(500).json({ success: false, message: err.message });
-    }
-  },
+exports.getAll = async (req, res) => {
+  try {
+    const docs = await PayrollPricing.find().lean();
+    res.json({ success: true, data: docs });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
 };
 
+exports.upsert = async (req, res) => {
+  try {
+    const data = req.body;
+    if (!data.serviceKey)
+      return res.status(400).json({ success: false, message: "serviceKey required" });
+    const doc = await PayrollPricing.findOneAndUpdate(
+      { serviceKey: data.serviceKey },
+      { $set: data },
+      { upsert: true, new: true, runValidators: true }
+    );
+    res.json({ success: true, data: doc });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+exports.remove = async (req, res) => {
+  try {
+    await PayrollPricing.deleteMany({});
+    res.json({ success: true, message: "All payroll pricing deleted" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
